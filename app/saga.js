a0672bccf94c5462b2834441cbb3f9aa9f6ce75b
Paul Hoskinson <plhosk@gmail.com>
2016-12-07 07:25:58 -0700

    disable action logging

--- app/saga.js
+++ app/saga.js
@@ -1,19 +1,19 @@
-import { take, select } from 'redux-saga/effects'
+// import { take, select } from 'redux-saga/effects'
 
 import chartsSagas from './charts/chartsSagas'
 
-// Log every Redux action
-function* logActions() {
-  while (true) { //eslint-disable-line
-    const action = yield take()
-    const state = yield select()
-    console.log('action:', action, 'state:', state) //eslint-disable-line
-  }
-}
+// // Log every Redux action
+// function* logActions() {
+//   while (true) { //eslint-disable-line
+//     const action = yield take()
+//     const state = yield select()
+//     console.log('action:', action, 'state:', state) //eslint-disable-line
+//   }
+// }
 
 export default function* rootSaga() {
   yield [
-    logActions(),
+    // logActions(),
     chartsSagas(),
   ]
 }
