55f62300f52cbed3fb0cdc55796754866ec61777
Paul Hoskinson <plhosk@gmail.com>
2016-12-08 00:27:36 -0700

    convert chart to PureComponent for performance

--- app/charts/LineChart.jsx
+++ app/charts/LineChart.jsx
@@ -4,98 +4,100 @@ import CircularProgress from 'material-ui/CircularProgress'
 
 import colors from './colors'
 
-const LineChart = (props) => {
-  const { symbol, prices, loading } = props
+class LineChart extends React.PureComponent { // eslint-disable-line
+  render() {
+    const { symbol, prices, loading } = this.props
 
-  const styles = {
-    div: {
-      maxWidth: 1000,
-      height: 150,
-      paddingBottom: 3,
-      margin: '0 auto',
-    },
-    loadingDiv: {
-      textAlign: 'center',
-      maxWidth: 700,
-      height: 100,
-      margin: '0 auto',
-      padding: 25,
-    },
-  }
+    const styles = {
+      div: {
+        maxWidth: 1000,
+        height: 150,
+        paddingBottom: 3,
+        margin: '0 auto',
+      },
+      loadingDiv: {
+        textAlign: 'center',
+        maxWidth: 700,
+        height: 100,
+        margin: '0 auto',
+        padding: 25,
+      },
+    }
 
-  if (loading) {
+    if (loading) {
+      return (
+        <div style={styles.loadingDiv}>
+          <CircularProgress size={60} thickness={7} /><br />
+          Loading...
+        </div>
+      )
+    }
+
+    if (prices.length === 0) {
+      return (
+        <div style={styles.loadingDiv}>
+          <br /><br />
+          Data unavailable
+        </div>
+      )
+    }
+
+    const data = {}
+    data.labels = prices.map(row => Date.parse(row[0]))
+    const values = prices.map(row => row[1])
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    data.datasets = []
+    data.datasets.push({
+      data: values,
+      label: symbol,
+      fill: true,
+      pointRadius: 0,
+      pointHitRadius: 30,
+      borderColor: color,
+      backgroundColor: color,
+    })
+
     return (
-      <div style={styles.loadingDiv}>
-        <CircularProgress size={60} thickness={7} /><br />
-        Loading...
-      </div>
-    )
-  }
-
-  if (prices.length === 0) {
-    return (
-      <div style={styles.loadingDiv}>
-        <br /><br />
-        Data unavailable
-      </div>
-    )
-  }
-
-  const data = {}
-  data.labels = prices.map(row => Date.parse(row[0]))
-  const values = prices.map(row => row[1])
-  const color = colors[Math.floor(Math.random() * colors.length)]
-  data.datasets = []
-  data.datasets.push({
-    data: values,
-    label: symbol,
-    fill: true,
-    pointRadius: 0,
-    pointHitRadius: 30,
-    borderColor: color,
-    backgroundColor: color,
-  })
-
-  return (
-    <div style={styles.div}>
-      <Line
-        data={data}
-        options={{
-          legend: { display: false },
-          maintainAspectRatio: false,
-          tooltips: {
-            mode: 'label',
-            callbacks: {
-              title: (tooltipItems, datas) => (
-                new Date(datas.labels[tooltipItems[0].index])
-                .toLocaleString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })
-              ),
-              label: (tooltipItems, datas) => (
-                `$${datas.datasets[0].data[tooltipItems.index]}`
-              ),
-            },
-          },
-          scales: {
-            yAxes: [{
-              gridLines: { drawOnChartArea: false },
-            }],
-            xAxes: [{
-              display: false,
-              ticks: {
-                callback: value => (
-                  new Date(value).toLocaleDateString('en-US', { month: 'short', year: 'numeric' })
+      <div style={styles.div}>
+        <Line
+          data={data}
+          options={{
+            legend: { display: false },
+            maintainAspectRatio: false,
+            tooltips: {
+              mode: 'label',
+              callbacks: {
+                title: (tooltipItems, datas) => (
+                  new Date(datas.labels[tooltipItems[0].index])
+                  .toLocaleString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })
+                ),
+                label: (tooltipItems, datas) => (
+                  `$${datas.datasets[0].data[tooltipItems.index]}`
                 ),
               },
-              type: 'time',
-              time: {
-                unit: 'month',
-              },
-            }],
-          },
-        }}
-      />
-    </div>
-  )
+            },
+            scales: {
+              yAxes: [{
+                gridLines: { drawOnChartArea: false },
+              }],
+              xAxes: [{
+                display: false,
+                ticks: {
+                  callback: value => (
+                    new Date(value).toLocaleDateString('en-US', { month: 'short', year: 'numeric' })
+                  ),
+                },
+                type: 'time',
+                time: {
+                  unit: 'month',
+                },
+              }],
+            },
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 LineChart.propTypes = {
